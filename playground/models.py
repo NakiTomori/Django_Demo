4045dfdbf33b630301b7395e4992f42cc698ceb8
NakiTomori <91220195+NakiTomori@users.noreply.github.com>
2023-09-15 14:36:53 +0700

    Delete playground directory

--- playground/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Blog(models.Model):
-    title = models.CharField(max_length= 300)
-    content = models.TextField(max_length= 1000)
-    image = models.URLField()
-
-    class Meta:
-        db_table = "tbBlog"
